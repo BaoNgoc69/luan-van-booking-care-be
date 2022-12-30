a6cf13b24455799d569e41da033916e55ad48f2b
BaoNgoc69 <109963495+BaoNgoc69@users.noreply.github.com>
2022-12-30 17:15:18 +0700

    init

---
+++ src/migrations/create-history.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('history', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            // patientId: DataTypes.INTEGER,
+            // doctorId: DataTypes.INTEGER,
+            // description: DataTypes.TEXT,
+            patientId: {
+                type: Sequelize.STRING
+            },
+            doctorId: {
+                type: Sequelize.INTEGER
+            },
+            description: {
+                type: Sequelize.TEXT
+            },
+
+
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('history');
+    }
+};
---
+++ src/seeders/seeder-user.js
@@ -0,0 +1,29 @@
+'use strict';
+// email: DataTypes.STRING,
+//     password: DataTypes.STRING,
+//     firstName: DataTypes.STRING,
+//     lastName: DataTypes.STRING,
+//     address: DataTypes.STRING,
+//     gender: DataTypes.BOOLEAN,
+//     roleid: DataTypes.STRING,
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('user', [{
+      email: 'baon57674@gmail.com',
+      password: '12345',
+      firstName: 'Bao',
+      lastName: 'Ngoc',
+      address: 'DT',
+      gender: '0',
+      typeRole: 'ROLE',
+      keyRole: 'R1',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('user', null, {});
+  }
+};
