a754c0afd5215d975e8c0377b90d2cc3f0b96426
BaoNgoc69 <109963495+BaoNgoc69@users.noreply.github.com>
2023-01-08 00:49:53 +0700

    done 36

--- src/server.js
+++ src/server.js
@@ -3,11 +3,47 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from './route/web';
+//import cors from 'cors';
 require('dotenv').config();
 
 let app = express();
-
+// let whitelist = ['http://localhost:3000'];
+// let corsOptions = {
+//     origin: (origin, callback) => {
+//         if (whitelist.indexOf(origin) !== -1) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     }, credentials: true
+// }
+//app.use(cors(corsOptions));
 //config app
+
+// function setCors() {
+
+//     this.app.use(cors(corsOptions));
+// }
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.URL_REACT);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -15,7 +51,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 viewEngine(app);
 initWebRoutes(app);
 
-let port = process.env.PORT || 8085;
+let port = process.env.PORT || 8081;
 //Port === undefined => port = 6969
 
 app.listen(port, () => {
