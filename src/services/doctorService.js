34c82e666b9713cdc2432cfefdf3485348275f01
BaoNgoc69 <109963495+BaoNgoc69@users.noreply.github.com>
2023-02-14 23:11:31 +0700

    done 74

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,5 +1,8 @@
-import { resolve } from "path";
 import db from "../models/index";
+require('dotenv').config();
+import _ from 'lodash';
+
+const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
 
 
 let getTopDoctorHome = (limitInput) => {
@@ -144,9 +147,69 @@ let getDetailDoctorById = (inputId) => {
         }
     })
 }
+
+let bulkCreateSchedule = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.arrSchedule || !data.doctorId || !data.formatedDate) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameter!!!'
+                })
+            }
+            else {
+                let schedule = data.arrSchedule;
+                if (schedule && schedule.length > 0) {
+                    schedule = schedule.map(item => {
+                        item.maxNumber = MAX_NUMBER_SCHEDULE;
+                        return item;
+                    })
+                }
+                console.log("bao ngoc: send data: ", schedule)
+                let existing = await db.schedule.findAll(
+                    {
+                        where: { doctorId: data.doctorId, date: data.formatedDate },
+                        attributes: ['timeType', 'date', 'doctorId', 'maxNumber'],
+                        raw: true
+                    }
+                )
+
+                if (existing && existing.length > 0) {
+                    existing = existing.map(item => {
+                        item.date = new Date(item.date).getTime();
+                        return item;
+                    })
+                }
+
+                let toCreate = _.differenceWith(schedule, existing, (a, b) => {
+                    return a.timeType === b.timeType && a.date === b.date;
+                })
+
+                if (toCreate && toCreate.length > 0) {
+                    await db.schedule.bulkCreate(toCreate);
+
+                }
+
+
+
+
+                resolve({
+                    errCode: 0,
+                    errMessage: 'ok'
+                })
+            }
+
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     saveDetailInforDoctor: saveDetailInforDoctor,
-    getDetailDoctorById: getDetailDoctorById
+    getDetailDoctorById: getDetailDoctorById,
+    bulkCreateSchedule: bulkCreateSchedule
 }
